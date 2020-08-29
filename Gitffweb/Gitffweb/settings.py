bf6c9954989dd4c9a1912cca05b688bccca88bdc
Mike <pection.naphat@gmail.com>
2020-08-30 05:27:32 +0700

    Webview

--- Gitffweb/Gitffweb/settings.py
+++ Gitffweb/Gitffweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0y%buv377v!)-cdmas3=gcb4i#ni)to%@j(4^32^op9bx_c+&r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
